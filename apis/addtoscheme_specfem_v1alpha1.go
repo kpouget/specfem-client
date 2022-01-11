05b26b85b426eb6170f3c720123af76861bcbece
Kevin Pouget <kpouget@redhat.com>
2022-01-11 11:10:08 +0100

    apis/addtoscheme_specfem_v1alpha1.go: use local package for Specfem API

--- apis/addtoscheme_specfem_v1alpha1.go
+++ apis/addtoscheme_specfem_v1alpha1.go
@@ -1,7 +1,7 @@
 package apis
 
 import (
-	"github.com/openshift-psap/specfem-client-api/specfem-api/pkg/apis/specfem/v1alpha1"
+	"github.com/openshift-psap/specfem-client/apis/specfem/v1alpha1"
 )
 
 func init() {
